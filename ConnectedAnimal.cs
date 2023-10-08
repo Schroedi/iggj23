56419c3bb28ebcaa00c3b8e55862a5bc44a8766a
Philip Trettner <trettner@shapedcode.com>
2023-10-08 13:33:19 +0200

    proper connected animals

--- ConnectedAnimal.cs
+++ ConnectedAnimal.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Security.Policy;
 
 public class ConnectedAnimal
 {
@@ -18,12 +19,59 @@ public class ConnectedAnimal
 
         var animals = new List<ConnectedAnimal>();
 
+        var connectedTo = new Dictionary<AnimalPhysics, List<AnimalPhysics>>();
+
+        void Connect(RigidBody2D rb0, RigidBody2D rb1)
+        {
+            var ap0 = rb0.GetParent() as AnimalPhysics;
+            var ap1 = rb1.GetParent() as AnimalPhysics;
+
+            if (!connectedTo.ContainsKey(ap0))
+                connectedTo.Add(ap0, new List<AnimalPhysics>());
+            if (!connectedTo.ContainsKey(ap1))
+                connectedTo.Add(ap1, new List<AnimalPhysics>());
+
+            connectedTo[ap0].Add(ap1);
+            connectedTo[ap1].Add(ap0);
+        }
+
+        // collect all AP -> AP connections
         foreach (var n in root.GetChildren())
             if (n is AnimalPhysics ap)
             {
+                if (!connectedTo.ContainsKey(ap))
+                    connectedTo.Add(ap, new List<AnimalPhysics>());
+                foreach (var nn in ap.GetChildren())
+                    if (nn is RigidBody2D rb)
+                        foreach (var nnn in rb.GetChildren())
+                            if (nnn is PinJoint2D pj)
+                                Connect(root.GetNode<RigidBody2D>(pj.NodeA), root.GetNode<RigidBody2D>(pj.NodeB));
+            }
+
+        var visited = new HashSet<AnimalPhysics>();
+
+        foreach (var n in root.GetChildren())
+            if (n is AnimalPhysics apRoot)
+            {
+                if (visited.Contains(apRoot))
+                    continue;
+
                 var ca = new ConnectedAnimal { };
-                ca.AllAnimals.Add(ap);
-                ca.TotalArea += ap.TotalArea;
+
+                void Visit(AnimalPhysics ap)
+                {
+                    if (visited.Contains(ap))
+                        return;
+
+                    ca.AllAnimals.Add(ap);
+                    ca.TotalArea += ap.TotalArea;
+                    visited.Add(ap);
+
+                    foreach (var app in connectedTo[ap])
+                        Visit(app);
+                }
+                Visit(apRoot);
+
                 animals.Add(ca);
             }
 
